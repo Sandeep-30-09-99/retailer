536199bc2a9114d5bd19f4f813861707f589b41a
Sandeep-30-09-99 <123sandeeplamba@gmail.com>
2022-07-07 09:17:45 +0530

    7 july 22

---
+++ app/src/main/java/com/rk/riggle_retailer/data/response/DummyResponse.kt
@@ -0,0 +1,6 @@
+package com.rk.riggle_retailer.data.response
+
+data class DummyResponse(
+    val no: String,
+    val name: String
+)
---
+++ app/src/main/java/com/rk/riggle_retailer/utils/SquareLinearLayout.java
@@ -0,0 +1,30 @@
+package com.rk.riggle_retailer.utils;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class SquareLinearLayout extends LinearLayout {
+    public SquareLinearLayout(Context context) {
+        super(context);
+    }
+
+    public SquareLinearLayout(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public SquareLinearLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public SquareLinearLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, widthMeasureSpec);
+    }
+}
